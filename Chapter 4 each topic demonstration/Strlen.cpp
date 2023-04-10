b8971aad6e891b010c28687279266644d8bd292d
123muba <129530664+123muba@users.noreply.github.com>
2023-04-10 11:35:03 +0300

    Create Strlen.cpp

---
+++ Chapter 4 each topic demonstration/Strlen.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int main()
+{
+char m1[50];
+
+cout<<"Enter the string "<<endl;
+cin>>m1;
+cout<<"Length is:"<<strlen(m1)<<endl;
+    return 0;
+}
+
+    
